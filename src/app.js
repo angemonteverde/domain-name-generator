c081bc93df3e6a0f70c6bb716ddc9a6b683ba032
angemonteverde <angemonteverde@gmail.com>
2023-01-13 19:26:17 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let exts = [".com"];
+  let textHTML = '<ul class="list-group">';
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < exts.length; m++) {
+          const element1 = pronoun[i];
+          const element2 = adj[j];
+          const element3 = noun[k];
+          const element4 = exts[m];
+          let domainName = element1 + element2 + element3 + element4;
+          textHTML += '<li class="list-group-item">' + domainName + "</li>";
+        }
+      }
+    }
+  }
+  // 1. Generar todas las combinaciones
+  // n. Mostrar una lista con todas las combinaciones
+  textHTML += "</ul>";
+  let element = document.querySelector("#domain");
+  element.innerHTML = textHTML;
   console.log("Hello Rigo from the console!");
 };
